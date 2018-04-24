fb3c1c7adc7f1d4c786629b0234b2e68fc88a3a8
Tiwaz <TiwazBiu@gmail.com>
2018-04-25 04:20:52 +0800

    开始工作

--- Project_Files/settings/Project.Debug.cspy.ps1
+++ Project_Files/settings/Project.Debug.cspy.ps1
@@ -23,9 +23,9 @@ param([String]$debugfile = "");
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\uav\1.26_U2OK\Project_Files\settings\Project.Debug.general.xcl" --backend -f "C:\uav\1.26_U2OK\Project_Files\settings\Project.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\yushe\Documents\GitHub\Flight-Control\Project_Files\settings\Project.Debug.general.xcl" --backend -f "C:\Users\yushe\Documents\GitHub\Flight-Control\Project_Files\settings\Project.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\uav\1.26_U2OK\Project_Files\settings\Project.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\uav\1.26_U2OK\Project_Files\settings\Project.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\yushe\Documents\GitHub\Flight-Control\Project_Files\settings\Project.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\yushe\Documents\GitHub\Flight-Control\Project_Files\settings\Project.Debug.driver.xcl" 
 }
